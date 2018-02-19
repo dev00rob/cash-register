1d6a0b9dcc5e64e4ec25bc7c89a4abcef79b41e7
Peter Horton <phorton@softstackfactory.org>
2018-02-19 23:50:30 +0000

    can finally get a sum of coins and bills with integrity

--- cashRegister.js
+++ cashRegister.js
@@ -18,55 +18,60 @@ change in coin and bills, sorted in highest to lowest order. Each time you
 get a small part of the register working create a commit to mark that moment 
 in time.
  */
-
 function checkCashRegister(price, cash, cid) {
-
+    let availableCash = getTotal(cid);
 }
 
-/*
-checkCashRegister(3.26,100,[
-    [ "PENNY" ,  1.01 ], 
-    [  "NICKEL" ,  2.05  ], 
-    [  "DIME" , 3.10  ], 
-    [ "QUARTER" ,  4.25 ], 
-    [ "ONE" ,  90 ],
-    [ "FIVE" ,  55 ], 
-    [  "TEN",   20  ], 
-    [  "TWENTY" ,  60  ], 
-    [  "ONE HUNDRED" ,  100  ] 
+function getTotal(drawer) {
+    let casho = 0;
+    for (let i = 0, x = drawer.length; i < x; i++) {
+        casho += parseInt((drawer[i][1] * 100).toFixed(0), 10);
+    }
+    return parseFloat((casho / 100).toFixed(2));
+}
+
+checkCashRegister(3.26, 100, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.10],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
 ]);
 
-/*
-  Should return: 
-  [ [ "TWENTY" , 60], [ "TEN" , 20], [ "FIVE" , 15], [ "ONE" , 1],
-    [ "QUARTER" , 0.5], [ "DIME" , 0.2], [ "PENNY" , 0.04] ]
- 
- 
-checkCashRegister(19.5,20, [
-    [ "PENNY" ,  0.01 ], 
-    [ "NICKEL" ,  0 ], 
-    [ "DIME" ,  0 ], 
-    [ "QUARTER",   0 ], 
-    [ "ONE" ,  0 ], 
-    [ "FIVE" ,  0 ], 
-    [ "TEN" ,  0 ], 
-    [ "TWENTY" ,  0], 
-    [ "ONE HUNDRED" ,  0 ]
-]);
+
+// Should return: 
+//   [ [ "TWENTY" , 60], [ "TEN" , 20], [ "FIVE" , 15], [ "ONE" , 1],
+//     [ "QUARTER" , 0.5], [ "DIME" , 0.2], [ "PENNY" , 0.04] ]
+
+
+// checkCashRegister(19.5, 20, [
+//     ["PENNY", 0.01],
+//     ["NICKEL", 0],
+//     ["DIME", 0],
+//     ["QUARTER", 0],
+//     ["ONE", 0],
+//     ["FIVE", 0],
+//     ["TEN", 0],
+//     ["TWENTY", 0],
+//     ["ONE HUNDRED", 0]
+// ]);
 
 // Should return: "Insufficient Funds"
 
-checkCashRegister(19.5,20,[
-    [ "PENNY" ,  0.5 ], 
-    [ "NICKEL" ,  0 ], 
-    [ "DIME" ,  0 ], 
-    [ "QUARTER" ,  0], 
-    [ "ONE" ,  0 ], 
-    [ "FIVE" ,  0 ], 
-    [ "TEN" ,  0 ], 
-    [ "TWENTY" ,  0], 
-    [ "ONE HUNDRED" ,  0 ]
-]);
+// checkCashRegister(19.5, 20, [
+//     ["PENNY", 0.5],
+//     ["NICKEL", 0],
+//     ["DIME", 0],
+//     ["QUARTER", 0],
+//     ["ONE", 0],
+//     ["FIVE", 0],
+//     ["TEN", 0],
+//     ["TWENTY", 0],
+//     ["ONE HUNDRED", 0]
+// ]);
 
 // Should return: "Closed"
- */
